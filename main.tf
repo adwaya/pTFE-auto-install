7aa9eba1cb282903258855de4f68489174ef0e4e
JoeStack <joern@tel2ip.net>
2018-10-30 07:39:45 +0100

    Update main.tf
    fixed private IP allocation

--- main.tf
+++ main.tf
@@ -121,7 +121,7 @@ resource "aws_instance" "tfe_node" {
   ami                         = "${data.aws_ami.ubuntu.id}"
   instance_type               = "t2.large"
   subnet_id                   = "${aws_subnet.tfe_subnet.id}"
-  private_ip                  = "${element(aws_subnet.tfe_subnet.*.id, count.index + 100)}"
+  private_ip                  = "${cidrhost(aws_subnet.tfe_subnet.cidr_block, count.index + 100)}"
   associate_public_ip_address = "true"
   vpc_security_group_ids      = ["${aws_security_group.tfe.id}"]
   key_name                    = "${var.key_name}"
